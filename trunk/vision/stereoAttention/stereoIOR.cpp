aa160019a6705b6d154def9926d1925860602107
niehaula <niehaula@f4f85414-d5a0-af94-c444-3e4a10407bf2>
2011-07-22 23:13:21 +0000

    updates to stereoattention system and egomapper modules, should both be testing ready now
    added stereoIOR module which handles inhibition of return 
    
    added some basic configuration files and scripts for running the demo on the icub

---
+++ trunk/vision/stereoAttention/stereoIOR.cpp
@@ -0,0 +1,486 @@
+/*
+ *  stereoIOR.cpp
+ *
+ * 	Logan Niehaus
+ * 	7/22/11
+ * 	module implementing some basic IOR functionality
+ *
+ * inputs:
+ *  	/stereoIOR/sal:l	-- unwarped left weighted salience sum (same as stereoAttn)
+ *  	/stereoIOR/sal:r	-- unwarped right weighted salience sum (same as stereoAttn)
+ *  	/stereoIOR/ior:i	-- ior event location as bottle of image coords [ul vl ur vr]
+ *  	/stereoIOR/pos:h	-- streaming head position, for remapping of old events
+ *
+ * params:
+ * 		decay	-- decay factor on event inhibition level (0-1, 0.01 is about 30s at 15fps)
+ * 		maxtime	-- maximum lifespan for an IOR event. once exceeded, event will be removed.
+ *		gauss	-- gaussian-style inhibition. arg specifies pixel width gaussian window (must be odd).
+ *		thresh	-- threshold style inhibition. arg specifies thresh level as % of peak
+ *					(nb gaussian and threshold inhibition behaviors are mutually exclusive)
+ *		name	-- module basename for ports (D /stereoIOR)
+ *		<cam parameters>		-- intrinsic calibration params see iKinGazeCtrl docs.
+ *
+ *
+ * outputs:
+ * 		/stereoIOR/ior:l	-- left IOR map for current image
+ * 		/stereoIOR/ior:r	-- right IOR map for current image
+ *
+ * TODO:   fix threshold-behavior for IOR
+ *
+ */
+
+//yarp includes
+#include <yarp/os/Network.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/RateThread.h>
+#include <yarp/os/Time.h>
+#include <yarp/os/Semaphore.h>
+#include <yarp/sig/Vector.h>
+#include <yarp/sig/Matrix.h>
+#include <yarp/sig/Image.h>
+#include <yarp/sig/ImageFile.h>
+#include <yarp/math/Math.h>
+#include <yarp/math/SVD.h>
+
+#include <iCub/iKin/iKinFwd.h>
+#include <iCub/ctrl/math.h>
+
+//external cv includes
+#include <cv.h>
+
+//misc inc
+#include <string>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <deque>
+
+//namespaces
+using namespace std;
+using namespace cv;
+using namespace yarp;
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::math;
+using namespace iCub::iKin;
+using namespace iCub::ctrl;
+
+#define PI 3.14159
+
+struct iorEvent {
+
+	int nframes;
+	double x, y, z;
+	double val;
+	int ul, vl, ur, vr;
+	bool unregistered;
+
+};
+
+class EventBuffer : public deque<iorEvent> {
+
+private:
+
+	Semaphore mutex;
+
+public:
+
+	void lock()   { mutex.wait(); }
+	void unlock() { mutex.post(); }
+
+};
+
+class iorPort : public BufferedPort<Bottle> {
+
+protected:
+
+	EventBuffer &b;
+
+public:
+
+	iorPort(EventBuffer & _b) : b(_b) {	}
+
+	//callback for incoming ior events
+	virtual void onRead(Bottle& iore) {
+
+		int ul = iore.get(0).asInt();
+		int vl = iore.get(1).asInt();
+		int ur = iore.get(2).asInt();
+		int vr = iore.get(3).asInt();
+
+		iorEvent E;
+		E.unregistered = true;
+		E.ul = ul; E.vl = vl; E.ur = ur; E.vr = vr;
+		b.lock();
+		b.push_back(E);
+		b.unlock();
+
+	}
+
+};
+
+class stereoIORThread : public RateThread
+{
+protected:
+
+	ResourceFinder &rf;
+	string name;
+
+	//ports
+	BufferedPort<ImageOf<PixelFloat> > *portSalL;
+	BufferedPort<ImageOf<PixelFloat> > *portSalR;
+	BufferedPort<ImageOf<PixelFloat> > *portSalLO;
+	BufferedPort<ImageOf<PixelFloat> > *portSalRO;
+	BufferedPort<yarp::sig::Vector> *portHAngIn;
+	iorPort * portEventIn;
+
+	//data objects
+	EventBuffer buf;
+	ImageOf<PixelFloat> lImgL;
+	ImageOf<PixelFloat> lImgR;
+
+	//kinematics data
+	iCubEye * eyeL;
+	iCubEye * eyeR;
+	Matrix Pl, Pr;
+	Matrix rtToL, rtToR;
+
+	//running parameters
+	double decay;
+	int maxtime;
+	int mode;
+	int grad;
+	double pthr;
+	double gscale;
+
+public:
+
+	stereoIORThread(ResourceFinder &_rf) : RateThread(50), rf(_rf)
+	{ }
+
+	virtual bool threadInit()
+	{
+
+		name=rf.check("name",Value("stereoIOR")).asString().c_str();
+		decay = rf.check("decay", Value(0.01)).asDouble();
+		maxtime = rf.check("maxtime", Value(300)). asDouble();
+
+		mode = -1;
+		if (rf.check("gauss")) {
+			mode = 0;
+			grad = rf.find("gauss").asDouble();
+			Mat G = getGaussianKernel(grad, (float)grad/5.0, CV_32F);
+			minMaxLoc(G, NULL, &gscale, NULL, NULL);
+			gscale = 1.0/(gscale*gscale);
+		}
+		if (rf.check("thresh")) {
+			pthr = rf.find("thresh").asDouble();
+			mode = 1;
+		}
+		if (mode < 0) {
+			printf("please specify an inhibition function type (gauss or thresh)\n");
+			return false;
+		}
+
+		//get camera calibration parameters
+		Pl = Matrix(3, 4); Pl.zero(); Pl(2,2) = 1.0;
+		Pr = Matrix(3, 4); Pr.zero(); Pr(2,2) = 1.0;
+		Bottle ipsL = rf.findGroup("CAMERA_CALIBRATION_LEFT");
+		Bottle ipsR = rf.findGroup("CAMERA_CALIBRATION_RIGHT");
+		if (ipsL.size() && ipsR.size()) {
+
+			Pl(0,2) = ipsL.find("w").asDouble()/2.0;
+			Pl(1,2) = ipsL.find("h").asDouble()/2.0;
+			Pl(0,0) = ipsL.find("fx").asDouble();
+			Pl(1,1) = ipsL.find("fy").asDouble();
+
+			Pr(0,2) = ipsR.find("w").asDouble()/2.0;
+			Pr(1,2) = ipsR.find("h").asDouble()/2.0;
+			Pr(0,0) = ipsR.find("fx").asDouble();
+			Pr(1,1) = ipsR.find("fy").asDouble();
+
+		}
+		else {
+
+			fprintf(stdout,"Could not find calibration parameters for one of the cameras\n");
+			return false;
+
+		}
+
+		//open up ports
+		portSalL=new BufferedPort<ImageOf<PixelFloat> >;
+		string portSallName="/"+name+"/sal:l";
+		portSalL->open(portSallName.c_str());
+
+		portSalR=new BufferedPort<ImageOf<PixelFloat> >;
+		string portSalrName="/"+name+"/sal:r";
+		portSalR->open(portSalrName.c_str());
+
+		portSalLO=new BufferedPort<ImageOf<PixelFloat> >;
+		string portSalloName="/"+name+"/sal:lo";
+		portSalLO->open(portSalloName.c_str());
+
+		portSalRO=new BufferedPort<ImageOf<PixelFloat> >;
+		string portSalroName="/"+name+"/sal:ro";
+		portSalRO->open(portSalroName.c_str());
+
+		portHAngIn=new BufferedPort<yarp::sig::Vector>;
+		string portHAngName="/"+name+"/pos:h";
+		portHAngIn->open(portHAngName.c_str());
+
+		portEventIn=new iorPort(buf);
+		string portIORName="/"+name+"/ior:i";
+		portEventIn->open(portIORName.c_str());
+		portEventIn->useCallback();
+
+		//set up kinematic chains
+		eyeL = new iCubEye("left");
+		eyeR = new iCubEye("right");
+		eyeL->setAllConstraints(false);
+		eyeR->setAllConstraints(false);
+		eyeL->releaseLink(0); eyeR->releaseLink(0);
+		eyeL->releaseLink(1); eyeR->releaseLink(1);
+		eyeL->releaseLink(2); eyeR->releaseLink(2);
+
+		return true;
+
+	}
+
+	virtual void registerEvent(iorEvent &E) {
+
+		//register the new iorEvent
+		E.nframes = 0;
+		E.val = max(lImgL.pixel(E.ul,E.vl),lImgR.pixel(E.ur,E.vr));
+
+		//get the event location
+		yarp::sig::Vector b(4);
+		yarp::sig::Vector Xp(3);
+		Matrix A(4,3);
+		Matrix Al = Pl;
+		Matrix Ar = Pr;
+		Al(0,2) = Al(0,2)-E.ul; Al(1,2) = Al(1,2)-E.vl;
+		Ar(0,2) = Ar(0,2)-E.ur; Ar(1,2) = Ar(1,2)-E.vr;
+		Al = Al*rtToL; Ar = Ar*rtToR;
+		for (int i = 0; i < 2; i++) {
+			b[i] = -Al(i,3); b[i+2] = -Ar(i,3);
+			for (int j = 0; j < 3; j++) {
+				A(i,j) = Al(i,j);
+				A(i+2,j) = Ar(i,j);
+			}
+		}
+		Xp = pinv(A)*b;
+		E.x = Xp[0]; E.y = Xp[1]; E.z = Xp[2];
+
+		E.unregistered = false;
+
+	}
+
+	virtual void run()
+	{
+
+		//first check to see if any new events need to be registered
+		for (int i = 0; i < buf.size(); i++) {
+
+			iorEvent &event = buf.at(i);
+			if (event.unregistered) {
+				registerEvent(event);
+			}
+
+		}
+
+
+		//get current head configuration
+		yarp::sig::Vector *headAng = portHAngIn->read(true);
+		yarp::sig::Vector angles(8); angles.zero();
+		angles[3] = (*headAng)[0]; angles[4] = (*headAng)[1];
+		angles[5] = (*headAng)[2]; angles[6] = (*headAng)[3];
+		angles[7] = (*headAng)[4] + (*headAng)[5]/2.0;
+		angles = PI*angles/180.0;
+		rtToL = SE3inv(eyeL->getH(angles));
+		angles[7] = PI*((*headAng)[4] - (*headAng)[5]/2.0)/180.0;
+		rtToR = SE3inv(eyeR->getH(angles));
+
+		//get current salience maps
+		ImageOf<PixelFloat> *pSalL = portSalL->read(false);
+		ImageOf<PixelFloat> *pSalR = portSalR->read(false);
+
+
+		if (pSalL && pSalR) {
+
+			//save the current maps
+			lImgL.copy(*pSalL); lImgR.copy(*pSalR);
+
+			ImageOf<PixelFloat> &loImg = portSalLO->prepare();
+			ImageOf<PixelFloat> &roImg = portSalRO->prepare();
+			loImg.copy(*pSalL); roImg.copy(*pSalR);
+			Mat Sl(loImg.height(), loImg.width(), CV_32F, (void *)loImg.getRawImage());
+			Mat Sr(roImg.height(), roImg.width(), CV_32F, (void *)roImg.getRawImage());
+			Mat ffMskL(loImg.height()+2, loImg.width()+2, CV_8UC1); ffMskL.setTo(Scalar(0));
+			Mat ffMskR(roImg.height()+2, roImg.width()+2, CV_8UC1); ffMskR.setTo(Scalar(0));
+			if (!mode) {
+				Sl.setTo(Scalar(0));
+				Sr.setTo(Scalar(0));
+			}
+
+			yarp::sig::Vector xyz(4), uvl(3), uvr(3);
+			int dcount = 0;
+			for (int i = 0; i < buf.size(); i++) {
+
+				//for each event, project its location onto the current image planes
+				iorEvent &tev = buf.at(i);
+				xyz[0] = tev.x; xyz[1] = tev.y; xyz[2] = tev.z; xyz[3] = 1;
+				uvl = Pl*rtToL*xyz;
+				uvr = Pr*rtToR*xyz;
+				uvl[0] = uvl[0]/uvl[2]; uvl[1] = uvl[1]/uvl[2];
+				uvr[0] = uvr[0]/uvr[2]; uvr[1] = uvr[1]/uvr[2];
+
+				//provide inhibition at the location
+				if (mode) {
+
+					//threshold-style inhibition (not yet functioning -- TODO)
+					if (uvl[0] > 0 && uvl[0] < pSalL->width() &&
+							uvl[1] > 0 && uvl[1] < pSalL->height()) {
+						//floodFill(Sl, ffMskL, Point((int)uvl[0], (int)uvl[1]), Scalar(255*exp(-tev.nframes*decay)),
+						//		NULL, Scalar((1-pthr)*tev.val), Scalar((1-pthr)*tev.val));
+						circle(Sl, Point((int)uvl[0], (int)uvl[1]), 30, Scalar(tev.val*exp(-tev.nframes*decay)), -1);
+
+					}
+					if (uvr[0] > 0 && uvr[0] < pSalR->width() &&
+							uvr[1] > 0 && uvr[1] < pSalR->height()) {
+						//floodFill(Sr, ffMskR, Point((int)uvr[0], (int)uvr[1]), Scalar(255*exp(-tev.nframes*decay)),
+						//		NULL, Scalar((1-pthr)*tev.val), Scalar((1-pthr)*tev.val));
+						circle(Sr, Point((int)uvr[0], (int)uvr[1]), 30, Scalar(tev.val*exp(-tev.nframes*decay)), -1);
+					}
+				}
+				else {
+
+					//gaussian style inhibition -- place a delta at each projected point
+					if (uvl[0] > 0 && uvl[0] < pSalL->width() &&
+							uvl[1] > 0 && uvl[1] < pSalL->height()) {
+						//Sl.at<float>(Point((int)uvl[0],(int)uvl[1])) = 255*exp(-tev.nframes*decay);
+						Sl.at<float>((int)uvl[1],(int)uvl[0]) = 255*exp(-tev.nframes*decay);
+					}
+					if (uvr[0] > 0 && uvr[0] < pSalR->width() &&
+							uvr[1] > 0 && uvr[1] < pSalR->height()) {
+						//Sr.at<float>(Point((int)uvr[0],(int)uvr[1])) = 255*exp(-tev.nframes*decay);
+						Sr.at<float>((int)uvr[1],(int)uvr[0]) = 255*exp(-tev.nframes*decay);
+					}
+				}
+
+				//check to see if the event should expire after this frame
+				if (tev.nframes > maxtime) {
+					dcount++;
+				} else {
+					tev.nframes++;
+				}
+
+			}
+
+			//do final processing on inhibition maps
+			if (mode) {
+				Sl.copyTo(Sl, ffMskL);
+				Sr.copyTo(Sr, ffMskR);
+			}
+			else {
+				GaussianBlur(Sl, Sl, Size(grad,grad), grad/5.0, grad/5.0);
+				GaussianBlur(Sr, Sr, Size(grad,grad), grad/5.0, grad/5.0);
+				Sl = Sl*gscale;
+				Sr = Sr*gscale;
+			}
+
+			//retire any ior events that are too old
+			buf.lock();
+			for (int i = 0; i < dcount; i++) {
+				buf.pop_front();
+			}
+			buf.unlock();
+
+			//write out inhibition to port
+			portSalLO->write();
+			portSalRO->write();
+
+		}
+
+	}
+
+	virtual void threadRelease()
+	{
+
+
+		portSalL->interrupt();
+		portSalR->interrupt();
+		portSalLO->interrupt();
+		portSalRO->interrupt();
+		portHAngIn->interrupt();
+		portEventIn->interrupt();
+
+		portSalL->close();
+		portSalR->close();
+		portSalLO->close();
+		portSalRO->close();
+		portHAngIn->close();
+		portEventIn->close();
+
+		delete portSalL, portSalR;
+		delete portSalLO, portSalRO;
+		delete portHAngIn;
+		delete portEventIn;
+
+	}
+
+};
+
+class stereoIORModule: public RFModule
+{
+
+protected:
+
+	stereoIORThread *thr;
+
+public:
+
+	stereoIORModule() { }
+
+	virtual bool configure(ResourceFinder &rf)
+	{
+
+		thr=new stereoIORThread(rf);
+		if (!thr->start())
+		{
+			delete thr;
+			return false;
+		}
+
+		return true;
+	}
+
+	virtual bool close()
+	{
+		thr->stop();
+		delete thr;
+
+		return true;
+	}
+
+	virtual double getPeriod()    { return 1.0;  }
+	virtual bool   updateModule() { return true; }
+};
+
+
+int main(int argc, char *argv[])
+{
+	Network yarp;
+
+	if (!yarp.checkNetwork())
+		return -1;
+
+	ResourceFinder rf;
+
+	rf.configure("ICUB_ROOT",argc,argv);
+
+	stereoIORModule mod;
+
+	return mod.runModule(rf);
+}
