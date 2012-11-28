39ef85dd06760f5094b328065ab7006cdc55f9f6
lydia.majure@gmail.com <lydia.majure@gmail.com@f4f85414-d5a0-af94-c444-3e4a10407bf2>
2012-11-28 20:10:00 +0000

    This is the correct header

--- visMotor/motorTools/SOM.h
+++ visMotor/motorTools/SOM.h
@@ -11,19 +11,18 @@
 using namespace yarp::sig;
 
 class SOM {
-private:
- double *activation; //1D array of activations
- //double **weights; //2D array of weights
- int N; //number of joint angles learned
- int K; //number of neurons in map
 
 public:
  SOM(int k,int n);
  ~SOM();
+ int N; //number of joint angles learned
+ int K; //number of neurons in map
+ double *activation; //1D array of activations
  double **weights; //2D array of weights
  int getState();
  void update(Vector* input,double step);
  double mapDist(int i,int winner);
+ void setVals(int k, int n, double val);
 };
 
 #endif /* SOM_H_ */
